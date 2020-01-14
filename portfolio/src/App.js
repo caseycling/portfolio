5a23a442147af90ade36180c95430b8bb930859a
Casey <caseyclinga@gmail.com>
2020-01-13 16:55:27 -0800

    Imported router and linked URLs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Router>
+        <Navbar />
+        <Route exact path="/">
+          <React.Fragment>
+          <Home />
+          </React.Fragment>
+        </Route>
+        <Route path="/Projects" component={Projects}/>
+        <Route path="/Contact" component={Contact}/>
+      </Router>
     </div >
   );
 }
