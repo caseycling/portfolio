1ab457521badf2e20e9dc097ab4438db289a8b13
Casey <caseyclinga@gmail.com>
2020-01-15 21:32:16 -0800

    Added padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Link } from 'react-router-dom';
-import Projects from '../pages/Projects'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
+import '../../src/index.css'
 
 const Navbar = () => {
   render()
   return (
-    <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/projects'>Projects</Link></li>
-      <li><Link to='/contact'>Contact</Link></li>
+    <ul >
+      <li><Link to='/' className="navLink">Home</Link></li>
+      <li><Link to='/projects' className="navLink">Projects</Link></li>
+      <li><Link to='/contact' className="navLink">Contact</Link></li>
     </ul>
   )
 }
 
-// function navStyle() {
-//     return( {
-//         color: "blue";
-//         backgroundImage: url('/background.jpg')
-//     })
+// let navStyle = () => {
+//   return ({
+//     listStyleType: "none",
+
+//     margin: "0",
+//     padding: "0"
+//   })
 // }
 
+// let linkStyle = {
+//     display: "block",
+//     width: "60px",
+//     marginLeft: "0px",
+//     :hover {
+//       color: "purple"
+//     }
+// }
 export default Navbar;
