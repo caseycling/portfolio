4a9038e5bccb809a469d72e43e7fffcccea99ed9
Casey <caseyclinga@gmail.com>
2020-01-16 20:49:00 -0800

    Imported logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Link } from 'react-router-dom';
-import '../../src/index.css'
+import Github from '../images/githubLogo.png'
+import Linkedin from '../images/linkeInLogo.png'
+
 
 const Navbar = () => {
   render()
   return (
+    <div>
     <ul >
-      <li><Link to='/' className="navLink">Home</Link></li>
+      <li><Link to='/' className="navLink" style={{marginTop: "35%"}}>Home</Link></li>
       <li><Link to='/projects' className="navLink">Projects</Link></li>
       <li><Link to='/contact' className="navLink">Contact</Link></li>
+      <li><a href="https://github.com/caseycling" target="_blank"> 
+        <img src={Github} alt="gitHub" style={{height: "50px", width: "50px", marginTop: "25px"}} /></a>
+      </li>
+      <li><a href="https://www.linkedin.com/in/casey-clingaman-b670b5177/" target="_blank">
+        <img src={Linkedin} alt="linkedIn" style={{height: "50px", width: "50px", marginTop: "25px" }}/></a>
+      </li>
     </ul>
+    <div>
+    
+    </div>
+    </div>
   )
 }
 
