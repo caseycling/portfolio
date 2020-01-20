d5b68dacde628d0a7ca21268b777b42b3ead2f69
Casey <caseyclinga@gmail.com>
2020-01-20 15:08:15 -0800

    Installed express

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,47 +4,31 @@ import { Link } from 'react-router-dom';
 import Github from '../images/githubLogo.png'
 import Linkedin from '../images/linkeInLogo.png'
 import Profile from '../images/profilePic.jpg';
+import Background from '../images/navbackground.jpg';
 
 
 const Navbar = () => {
   render()
   return (
-    <div>
-    <ul style={{borderRight: "2px groove black"}}>
-      <li style={{fontSize: "24px", marginTop: "30%", fontStyle: "bold",  textShadow: "2px 2px 4px #000000"}}>Casey Clingaman</li>
-    <li><img src={Profile} alt="project" style={{height: "120px", width: "120px", marginTop: "25px", border: "3px solid black", borderRadius: "20px" }}/></li>
-      <li><Link to='/' className="navLink" style={{marginTop: "205"}}>Home</Link></li>
-      <li><Link to='/projects' className="navLink">Projects</Link></li>
-      <li><Link to='/contact' className="navLink">Contact</Link></li>
-      <li><a href="https://github.com/caseycling" target="_blank" rel="noopener noreferrer"> 
-        <img src={Github} alt="gitHub" style={{height: "50px", width: "50px", marginTop: "25px"}} /></a>
-      </li>
-      <li><a href="https://www.linkedin.com/in/casey-clingaman-b670b5177/" target="_blank" rel="noopener noreferrer">
-        <img src={Linkedin} alt="linkedIn" style={{height: "50px", width: "50px", marginTop: "25px" }}/></a>
-      </li>
-    </ul>
-    <div>
-    
-    </div>
+    <div >
+      <ul style={{ borderRight: "2px groove black" }}>
+        <li style={{ fontFamily: 'verdana', fontSize: "24px", marginTop: "30%", fontStyle: "bold", textShadow: "1px 3px #000000" }}>Casey Clingaman</li>
+        <li><img src={Profile} alt="project" style={{ height: "120px", width: "100px", marginTop: "25px", border: "3px solid black", borderRadius: "20px" }} /></li>
+        <li><Link to='/' className="navLink" style={{ marginTop: "205" }}>Home</Link></li>
+        <li><Link to='/projects' className="navLink">Projects</Link></li>
+        <li><Link to='/contact' className="navLink">Contact</Link></li>
+        <li><a href="https://github.com/caseycling" target="_blank" rel="noopener noreferrer">
+          <img src={Github} alt="gitHub" style={{ height: "50px", width: "50px", marginTop: "25px" }} /></a>
+        </li>
+        <li><a href="https://www.linkedin.com/in/casey-clingaman-b670b5177/" target="_blank" rel="noopener noreferrer">
+          <img src={Linkedin} alt="linkedIn" style={{ height: "50px", width: "50px", marginTop: "25px" }} /></a>
+        </li>
+      </ul>
+      <div>
+
+      </div>
     </div>
   )
 }
 
-// let navStyle = () => {
-//   return ({
-//     listStyleType: "none",
-
-//     margin: "0",
-//     padding: "0"
-//   })
-// }
-
-// let linkStyle = {
-//     display: "block",
-//     width: "60px",
-//     marginLeft: "0px",
-//     :hover {
-//       color: "purple"
-//     }
-// }
 export default Navbar;
