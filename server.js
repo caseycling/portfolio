c507810154b624eb8ce7916abb323815c533b144
Casey <caseyclinga@gmail.com>
2020-01-26 13:28:05 -0800

    Succesfully implemented sendgrid

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 var app = express();
+const SgMail = require('@sendgrid/mail');
+
+SgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 var PORT = process.env.PORT || 5000;
 
-
-app.get('/', (req, res) => {
-    res.send('Hello Express')
-});
+app.post('/contact', (req, res) => {
+    console.log(req.body)
+    SgMail.send(req.body)
+    .then(res => console.log(`SERVER.JS RESPONSE: ${res.body}`))
+    .catch(err => console.log(`SERVER.JS ERROR: ${err}`));
+  });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
