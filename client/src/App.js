5b0f042a9191d5f21a09d38e8e07a19d011227c6
Casey <caseyclinga@gmail.com>
2020-02-05 22:41:04 -0800

    Removed react router

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,15 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/projects" component={Projects}/>        
-        <Route path="/contact" component={Contact}/>
-      </Router>
+        <Home />
+        <Projects />
+        <Contact />
+       
     </div >
   );
 }
