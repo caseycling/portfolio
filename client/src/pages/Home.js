f9425c6e0b793797fd71a02f8d1ae6e9f2d09449
Casey <caseyclinga@gmail.com>
2020-02-05 23:16:56 -0800

    Edit about-me

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 		<div className="homeContainer" id="home">
 			<h1 id="homeHeader">Hi! Im Casey</h1>
 			<h5 id="homeTitle">FULL-STACK DEVELOPER</h5>
-			<p style={{ marginRight: "10%", fontSize: "20px", fontFamily: "fantasy" }}>Full-stack web developer with complete proficiency in fron-end and backend. Experienced with browser-based technologies as well as databases and server-side development. <br/> <br/>
+			<p style={{ marginRight: "10%", fontSize: "20px", fontFamily: "fantasy" }}> Dynamic web developer, proficient with front-end and-backend technologies, database management as well as server-side development. <br/> <br/>
 		
 			I am passionate about thoughtfully designed web-based applications with superior user experience in mind, turning ideas into reality.</p>
 		</div>
