ee9aec59437344de53da0f8cccc04fd0301e45e0
Casey <caseyclinga@gmail.com>
2020-02-05 22:42:11 -0800

    Converted navbar for react-scroll

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from '@testing-library/react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 import Github from '../images/githubLogo.png'
 import Linkedin from '../images/linkeInLogo.png'
 import Profile from '../images/profilePic.jpg';
 import Background from '../images/navbackground.jpg';
 
-const Navbar = () => {
-  render()
-  return (
-    <div className="navContainer">
-      <div style={{ background: Background }}>
-        <ul className="navList">
-          <li className="navName">Casey Clingaman</li>
-          <li><img src={Profile} alt="project" className="navImage" /></li>
-          <li><Link to='/' className="navLink">Home</Link></li>
-          <li><Link to='/projects' className="navLink">Projects</Link></li>
-          <li><Link to='/contact' className="navLink">Contact</Link></li>
-          <div className="logoContainer">
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navContainer">
+        <div style={{ background: Background }}>
+          <ul className="navList">
+            <li className="navName">Casey Clingaman</li>
+            <li><img src={Profile} alt="project" className="navImage" /></li>
             <li>
-              <a href="https://github.com/caseycling" target="_blank" rel="noopener noreferrer">
-                <img src={Github} alt="gitHub" id="navLogo" /></a>
+              <Link
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="navLink">
+                Home
+            </Link>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/casey-clingaman-b670b5177/" target="_blank" rel="noopener noreferrer">
-                <img src={Linkedin} alt="linkedIn" id="navLogo" /></a>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="navLink">
+                Projects
+            </Link>
             </li>
-          </div>
-        </ul>
+            <li>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="navLink">
+                Contact
+            </Link>
+            </li>
+            <div className="logoContainer">
+              <li>
+                <a href="https://github.com/caseycling" target="_blank" rel="noopener noreferrer">
+                  <img src={Github} alt="gitHub" id="navLogo" /></a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/casey-clingaman-b670b5177/" target="_blank" rel="noopener noreferrer">
+                  <img src={Linkedin} alt="linkedIn" id="navLogo" /></a>
+              </li>
+            </div>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Navbar;
