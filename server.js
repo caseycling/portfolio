87c64cdc1db5505b9e07d02dcaeca56457f7f910
Casey <caseyclinga@gmail.com>
2020-02-17 15:03:52 -0800

    Implemented new email API

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const express = require('express');
 var app = express();
-const SgMail = require('@sendgrid/mail');
 const path = require('path');
+var cors = require('cors');
 require('dotenv').config();
-
-
-SgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
-console.log(process.env.REACT_APP_SENDGRID_API_KEY)
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 //Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
-app.post('/contact', (req, res) => {
-    console.log(req.body) 
-    SgMail.send(req.body)
-    .then(res => console.log(`SERVER.JS RESPONSE: ${res}`))
-    .catch(err => console.log(`SERVER.JS ERROR: ${err}`));
-  });
+var DOMAIN = 'sandboxdc3ad469df4c4d7bb20c78a9d0c58608.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: process.env.REACT_APP_MAILGUN_API_KEY, domain: DOMAIN});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.post('/contact', (req, res) => {
+  console.log(req.body)
+  mailgun.messages().send(req.body, (error, body) => {
+    console.log(body)
+    console.log((error ? error : ''))
+  })
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))   
