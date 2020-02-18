53defe579dc9d94ae0475a1df4a7cf6752875ec4
Casey <caseyclinga@gmail.com>
2020-02-18 14:56:54 -0800

    Cleaned up the code

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
-// import barter from '../images/barter.png';
-// import gravel from '../images/gravel.png';
-
 import CarouselTest from '../components/Carousel';
 
 const Projects = () => {
 
     render()
     return (
-        <div className="projectsContainer" id="projects" style={{paddingBottom: "40px" }}>
-            <h3 style={{ fontSize: "30px", fontFamily: "MontSerrat,Tahoma,Arial,sans-serif", paddingTop: "0px", marginLeft: "30px", textAlign: "justify"  }}>Stuff I've Built</h3>
-            <div className="individualContainer" style={{ width: "90%", marginLeft: "10px", padding: "35px 20px 20px 20px", marginTop: "0px", border: "2px solid silver" }}>
-                <CarouselTest 
+        <div className="projectsContainer" id="projects" style={{ paddingBottom: "40px" }}>
+            <h3 style={ HeaderStyle }>Stuff I've Built</h3>
+            <div className="individualContainer" style={ CarouselStyle }>
+                <CarouselTest
                     autoplay={true}
                 />
             </div>
@@ -21,5 +17,22 @@ const Projects = () => {
     )
 }
 
+//Styling objects
+
+let HeaderStyle = { 
+    fontSize: "30px", 
+    fontFamily: "MontSerrat,Tahoma,Arial,sans-serif", 
+    paddingTop: "0px", 
+    marginLeft: "30px", 
+    textAlign: "justify" 
+}
+
+let CarouselStyle = {
+    width: "90%", 
+    marginLeft: "10px", 
+    padding: "35px 20px 20px 20px", 
+    marginTop: "0px", 
+    border: "2px solid silver"
+}
 
 export default Projects;
