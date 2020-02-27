4f60a2909fec02655735dc9a469fde4b09d2c306
Casey <caseyclinga@gmail.com>
2020-02-26 20:20:49 -0800

    Rearranged folder structure for cleaner code

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,27 @@
+
+
+import React from 'react';
+import '../../App.css';
+import Navbar from '../../components/Navbar/Navbar'
+import About from '../../components/About/About';
+import Contact from '../../components/Contact/Contact.js';
+import Projects from '../../components/Projects/Projects';
+import Technologies from '../../components/Technologies/Technologies.js'
+
+function Home() {
+  return (
+    <div className="home">
+        <Navbar />
+        <About />
+        <Technologies />
+        <Projects />
+        <Contact />
+       
+    </div >
+  );
+}
+
+export default Home;
+
+
+
