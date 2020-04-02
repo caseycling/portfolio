42fade2c469b401f6ee7fbf7e7e6da7b9b350015
Casey <caseyclinga@gmail.com>
2020-04-02 13:36:27 -0700

    Incorporated footer component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,7 +4,8 @@ import NavagationBar from '../../components/Navbar/Navbar'
 import About from '../../components/About/About';
 import Contact from '../../components/Contact/Contact.js';
 import Projects from '../../components/Projects/Projects';
-import Technologies from '../../components/Technologies/Technologies.js'
+import Technologies from '../../components/Technologies/Technologies.js';
+import Footer from '../../components/Footer/Footer.js'
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
         <Technologies />
         <Projects />
         <Contact />
+        <Footer />
        
     </div >
   );
