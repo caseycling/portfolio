9d828e8e2f4edd0315c65d73880372917e67458b
Casey <caseyclinga@gmail.com>
2020-04-02 13:35:16 -0700

    Created footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Footer.css'
+class Footer extends Component {
+    render() {
+        return (
+            <div className='footer-container'>
+                <p className='footer' >Casey Clingaman &copy; 2020</p>
+            </div>
+        )
+    }
+}
+
+export default Footer;
