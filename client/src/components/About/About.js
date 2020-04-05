e2f6954a8e7956b75a70b68d11ea9187c8d198e0
Casey <caseyclinga@gmail.com>
2020-04-04 18:52:28 -0700

    Added media queries

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,10 +5,11 @@ import './About.css'
 const Home = () => {
 	render()
 	return (
-		<div className="homeContainer" id="home">
-			<div className="contentContainer">
-				<h1 id="homeHeader">CASEY CLINGAMAN</h1>
-				<p style={{ margin: "30px 5%", fontSize: "40px",  fontFamily: '"Maison Neue", Helvetica Neue, Helvetica, Arial, sans-serif', textAlign: "center", lineHeight: "1.30769em" }}> I'm a Seattle based web designer & front-end developer focused on clean, efficient and robust code.</p>
+		<div className="home-container" id="home">
+			<div className="content-container">
+				<h1 id="home-header">CASEY CLINGAMAN</h1>
+				<p className="home-content"> 
+				I'm a Seattle based web designer & front-end developer focused on clean, efficient and robust code.</p>
 			</div>
 			<div style={{display: "flex", justifyContent: "center"}}>
 				<button className="about-btn">Learn more</button> 
