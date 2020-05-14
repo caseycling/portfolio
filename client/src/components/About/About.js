8d93c20c1219e6325b4918b4a9fb6eeb8b9f7c9e
Casey <caseyclinga@gmail.com>
2020-05-14 15:35:21 -0700

    Added scroll functionality to Learn More button

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import { render } from '@testing-library/react';
 import './About.css'
 
@@ -8,13 +9,23 @@ const Home = () => {
 		<div className="home-container" id="home">
 			<div className="content-container">
 				<h1 id="home-header">CASEY CLINGAMAN</h1>
-				<p className="home-content"> 
-				I'm a Seattle based web designer & front-end developer focused on clean, efficient and robust code.</p>
+				<p className="home-content">
+					I'm a Seattle based web designer & front-end developer focused on clean, efficient and robust code.</p>
 			</div>
-			<div style={{display: "flex", justifyContent: "center"}}>
-				<button className="about-btn">Learn more</button> 
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<button className="about-btn">
+					<Link
+						activeClass="active"
+						to="technologies"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						> Learn more
+					</Link>
+				</button>
 			</div>
-			
+
 		</div>
 	)
 }
